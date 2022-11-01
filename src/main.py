1c2da1286b4db646f300c0037992659364b92d80
Adithya Bhaskar <zer0.h6ck@gmail.com>
2022-11-01 14:17:11 +0530

    heatmaps done

--- src/main.py
+++ src/main.py
@@ -3,20 +3,42 @@ Adithya, Richeek, Aaron, 2022.
 This file has the main driver code. Does not provide any further functionality by itself.
 """
 
+import enum
 from globals import *
 from vars import Generator
 from optim import monte_carlo_simulation
 
 import os
 import json
+import math
 import warnings
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
     # warnings.filterwarnings("ignore")
     performance_map = {}
     if os.path.exists("data/perf-single.json"):
         performance_map = json.load(open("data/perf-single.json"))
+
+    num_ns = len(n_values)
+    num_ss = len(sparsity_values)
     
+    plot = np.zeros((num_ns, num_ss))
+    for i, n in enumerate(n_values):
+        for j, s in enumerate(sparsity_values):
+            plot[i,j] = performance_map[str(n)][str(s)]['rrmse']*math.sqrt(1000)
+    
+    h = sns.heatmap(plot, yticklabels=n_values, xticklabels=sparsity_values, \
+        annot=True)
+    h.set(ylabel='Number of measurements', \
+        xlabel='Fraction of people infected', \
+        title='RMSE of unweighted LASSO')
+    plt.savefig('data/plots/rmse-unweighted.png')
+    
+    return
     for n in n_values:
         nstr = str(n)
         if nstr not in performance_map:
